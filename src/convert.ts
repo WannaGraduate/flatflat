554fb75509c7c0ef7f8a1c25b08316f96d2f7b40
h4l-yup <sykim940@gmail.com>
2020-06-11 02:34:10 +0900

    Add: convert

---
+++ src/convert.ts
@@ -0,0 +1,110 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { TagInfo } from './interface/tag-info.interface';
+
+// treeToTag에서 파일명을 절대경로가 아니라 상대경로로 넣기 위해 필요
+let rootDir = '';
+
+/**
+ * workspace 경로를 받아 해당 경로 하단에 /.vscode/file-tag-system.json을 만들어주는 함수
+ * @param workspacePath workspace 경로
+ */
+export function convert(workspacePath: string) {
+  rootDir = workspacePath;
+
+  if (!fs.existsSync(path.join(workspacePath, 'converted'))) {
+      fs.mkdirSync(path.join(workspacePath, 'converted'));
+    }
+  const tagInfo = treeToTag(workspacePath, ['.git', '.vscode']);
+  fs.writeFileSync(
+    path.join(workspacePath, 'converted', 'file-tag-system.json'),
+    JSON.stringify(tagInfo, null, 4)
+  );
+}
+
+/**
+ * 경로를 받아 TagInfo 형태로 만들어주는 함수
+ * @param folderPath 태그로 만들 경로
+ * @param ignores 무시해야 하는 파일/폴더명
+ */
+function treeToTag(folderPath: string, ignores: string[]): TagInfo {
+  const fileList = fs.readdirSync(folderPath);
+  const [tag] = folderPath.split(path.sep).slice(-1);
+  const result: TagInfo = {
+    version: 1,
+    groups: {},
+  };
+  const updatedIgnores = getIgnores(folderPath, ignores);
+
+  for (const file of fileList) {
+    const uri = path.join(folderPath, file);
+    const stat = fs.lstatSync(uri);
+    if (updatedIgnores.indexOf(file) === -1) {
+      if (stat.isDirectory()) {
+        // uri 끝이 디렉토리이면 하나 상단 디렉토리가 태그 그룹명이 되고 디렉토리는 태그 그룹 안의 태그가 됨
+        if (result.groups[tag]) {
+          result.groups[tag].tags.push(file);
+        } else {
+          result.groups[tag] = {
+            type: 'any',
+            tags: [file],
+          };
+        }
+
+        // 재귀 호출
+        const { groups } = treeToTag(uri, updatedIgnores);
+
+        // 재귀 호출 결과 합치기
+        Object.keys(groups).forEach((groupName) => {
+          if (result.groups[groupName]) {
+            result.groups[groupName].tags = result.groups[
+              groupName
+            ].tags.concat(groups[groupName].tags);
+          } else {
+            result.groups[groupName] = groups[groupName];
+          }
+        });
+      } else {
+        const relativeDir = path.relative(rootDir, uri);
+        const convertedFileName = relativeDir.split(path.sep).join('.');
+        const convertedPath = path.join(rootDir, 'converted');
+
+        fs.copyFileSync(
+          uri,
+          path.join(convertedPath, convertedFileName)
+        );
+      }
+    }
+  }
+
+  result.groups = Object.keys(result.groups).reduce(
+    (groups: TagInfo['groups'], groupName) => {
+      if (result.groups[groupName].tags.length === 1) {
+        return groups;
+      } else {
+        groups[groupName] = result.groups[groupName];
+
+        return groups;
+      }
+    },
+    {}
+  );
+
+  return result;
+}
+
+/**
+ * 무시할 파일/폴더 리스트 만들기
+ * @param folderPath .gitignore 가져올 경로
+ * @param ignores 직접 추가할 ignore
+ */
+function getIgnores(folderPath: string, ignores: string[]) {
+  let gitIgnores: string[] = [];
+  if (fs.existsSync(path.join(folderPath, '.gitignore'))) {
+    gitIgnores = fs
+      .readFileSync(path.join(folderPath, '.gitignore'), 'utf8')
+      .split('\n');
+  }
+  return gitIgnores.concat(ignores);
+}
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
+import { convert } from './convert';
 import { FileProvider } from './file-provider';
 import { QueryProvider } from './query-provider';
 import { getTagGroups } from './tag-group';
@@ -9,66 +10,61 @@ import { getTagGroups } from './tag-group';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    const tagGroupJsonPath = path.join(vscode.workspace.rootPath!, 'file-tag-system.json');
-    if (!fs.existsSync(tagGroupJsonPath)) {
-        fs.writeFileSync(tagGroupJsonPath, JSON.stringify({
-            version: 1,
-            groups: {
-                example: {
-                    type: 'any',
-                    tags: [],
-                },
-            }
-        }, null, 4));
-    }
-
-    const fileProvider = new FileProvider(vscode.workspace.rootPath!);
-    vscode.window.registerTreeDataProvider('files', fileProvider);
-    vscode.commands.registerCommand('files.openFile', (resource) =>
-        vscode.window.showTextDocument(resource),
-    );
-    const queryProvider = new QueryProvider();
-    vscode.window.registerTreeDataProvider('queries', queryProvider);
-    context.subscriptions.push(
-        vscode.commands.registerCommand('queries.addGroup', async () => {
-            const quickPick = vscode.window.createQuickPick();
-
-            quickPick.items = getTagGroups().map((label) => ({ label }));
-            quickPick.onDidChangeSelection((selection) => {
-                if (
-                    queryProvider.queryList.findIndex((tag) => {
-                        return tag === selection[0].label;
-                    }) !== -1
-                ) {
-                    vscode.commands.executeCommand(
-                        'queries.delete',
-                        selection[0].label,
-                    );
-                } else {
-                    queryProvider.queryList = queryProvider.queryList.concat([
-                        selection[0].label,
-                    ]);
-
-                    fileProvider.queries = queryProvider.queryList;
-                    queryProvider.refresh();
-                    fileProvider.refresh();
-                }
-            });
-            quickPick.onDidHide(() => quickPick.dispose());
-            quickPick.show();
-        }),
-    );
-    vscode.commands.registerCommand('queries.delete', (groupName) => {
-        const index = queryProvider.queryList.findIndex((tagGroup) => {
-            return tagGroup === groupName;
-        });
-        if (index !== -1) {
-            queryProvider.queryList.splice(index, 1);
-            fileProvider.queries = queryProvider.queryList;
-        }
-        queryProvider.refresh();
-        fileProvider.refresh();
+    vscode.commands.registerCommand('Convert', () => {
+        convert(vscode.workspace.rootPath!);
     });
+
+    if (!fs.existsSync(path.join(vscode.workspace.rootPath!, 'file-tag-system.json'))) {
+        vscode.window.showInformationMessage('No file-tag-system.json.');
+    } else {
+        const fileProvider = new FileProvider(vscode.workspace.rootPath!);
+        vscode.window.registerTreeDataProvider('files', fileProvider);
+        vscode.commands.registerCommand('files.openFile', (resource) =>
+            vscode.window.showTextDocument(resource),
+        );
+        const queryProvider = new QueryProvider();
+        vscode.window.registerTreeDataProvider('queries', queryProvider);
+        context.subscriptions.push(
+            vscode.commands.registerCommand('queries.addGroup', async () => {
+                const quickPick = vscode.window.createQuickPick();
+
+                quickPick.items = getTagGroups().map((label) => ({ label }));
+                quickPick.onDidChangeSelection((selection) => {
+                    if (
+                        queryProvider.queryList.findIndex((tag) => {
+                            return tag === selection[0].label;
+                        }) !== -1
+                    ) {
+                        vscode.commands.executeCommand(
+                            'queries.delete',
+                            selection[0].label,
+                        );
+                    } else {
+                        queryProvider.queryList = queryProvider.queryList.concat([
+                            selection[0].label,
+                        ]);
+
+                        fileProvider.queries = queryProvider.queryList;
+                        queryProvider.refresh();
+                        fileProvider.refresh();
+                    }
+                });
+                quickPick.onDidHide(() => quickPick.dispose());
+                quickPick.show();
+            }),
+        );
+        vscode.commands.registerCommand('queries.delete', (groupName) => {
+            const index = queryProvider.queryList.findIndex((tagGroup) => {
+                return tagGroup === groupName;
+            });
+            if (index !== -1) {
+                queryProvider.queryList.splice(index, 1);
+                fileProvider.queries = queryProvider.queryList;
+            }
+            queryProvider.refresh();
+            fileProvider.refresh();
+        });
+    }
 }
 
 // this method is called when your extension is deactivated
